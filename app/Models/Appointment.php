aed0777c6641f7ccaed49fbdb1a49b7701898fd4
lanaorton <lana.orton@talentinnovations.co.uk>
2024-03-25 22:35:59 +0000

    Appointment migration and model, env for testing

---
+++ app/Models/Appointment.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'date_time',
+        'issue',
+        'contact_number',
+        'email_address',
+        'approved',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'date_time'     => 'datetime',
+            'name'          => 'string',
+            'email_address' => 'string',
+            'issue'         => 'string',
+            'contact_number' => 'string',
+            'approved'      => 'boolean',
+        ];
+    }
+}
---
+++ database/migrations/0001_01_01_000003_create_appointment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointment', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('date_time');
+            $table->longText('issue');
+            $table->string('contact_number');
+            $table->string('email_address');
+            $table->boolean('approved');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointment');
+    }
+};
